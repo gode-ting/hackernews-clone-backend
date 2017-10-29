c22ec9406922983c0352577a8c4e8763341362ed
Anders Bjergfelt <abj@dis-play.dk>
2017-10-29 16:52:47 +0100

    post crud almost done

--- src/main/java/controllers/PostController.java
+++ src/main/java/controllers/PostController.java
@@ -9,9 +9,16 @@ import com.daef.models.Post;
 import com.daef.repositories.PostRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  *
@@ -32,5 +39,43 @@ public class PostController {
        return posts;
      
     }
+    
+    //CREATE
+    //TESTING
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> createUser(@RequestBody Post post,UriComponentsBuilder ucBuilder) {
+        System.out.println("Creating Post " + post.userName);
+ 
+        repository.save(post);
+ 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/post/{id}").buildAndExpand(post.id).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+   
+    //READ
+     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Post getPost(@PathVariable("id") String id){
+		
+        return repository.findOne(id);
+    }
+    
+    //UPDATE
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Post updatePost(@ModelAttribute("post") Post post){
+		
+        return repository.insert(post);
+    }
+   
+    //DELETE
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String removePerson(@PathVariable("id") String id){
+		
+       repository.delete(id);
+        return "redirect:/persons";
+    }
+    
+    
     
 }
