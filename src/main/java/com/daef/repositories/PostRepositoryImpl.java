42d1c0db3c52d744636a270d15fd908e07eede3d
lalelarsen <frederikblarsen@hotmail.com>
2017-10-31 10:48:40 +0100

    done with this branch

--- src/main/java/com/daef/repositories/PostRepositoryImpl.java
+++ src/main/java/com/daef/repositories/PostRepositoryImpl.java
@@ -7,7 +7,9 @@ package com.daef.repositories;
 
 import com.daef.models.Post;
 import com.mongodb.WriteResult;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -23,15 +25,31 @@ public class PostRepositoryImpl implements PostInterface {
     
     @Override
     public void getAllPostByParentID(int id) {
+        //the query to use
         Query query = new Query(Criteria.where("PostParent").is(id));
         
+        //the end result. adds to this object, then returns it.
+        List<Post> result = new ArrayList();
+        
+        //the first level of comments. the original comments on the post
+        //NOT any comments on comments. yet.
         List<Post> list = mongoTemplate.find(query, Post.class);
         
-        System.out.println("size: " + list.size());
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).PostTitle);
+        //a stack to easily add and remove from.
+        Stack<Post> stack = new Stack();
+        stack.addAll(list);
+        
+        //loop until the stack is empty
+        while(!stack.isEmpty()){
+            Post p = stack.pop();
+            result.add(p);
+            query = new Query(Criteria.where("PostParent").is(p.HanesstID));
+            List<Post> tmpList = mongoTemplate.find(query, Post.class);
+            stack.addAll(tmpList);
+            
         }
-        System.out.println("done");
+        
+        
     }
     
 }
