0869a985590745235d10febb57c9e1a158312614
Anders Bjergfelt <abj@dis-play.dk>
2017-11-01 19:45:21 +0100

    travis

--- src/main/java/com/daef/Application.java
+++ src/main/java/com/daef/Application.java
@@ -32,11 +32,11 @@ public class Application implements CommandLineRunner {
     public void run(String... args) throws Exception {
 //        user.deleteAll();
 //        repository.deleteAll();
-        //user.save(new ApplicationUser("admin", "password"));
-        //user.save(new ApplicationUser("admin2", "password"));
+        user.save(new ApplicationUser("admin", "password"));
+        user.save(new ApplicationUser("admin2", "password"));
 //        // save a couple of articles
-        //repository.save(new Post("Charlie", "story", "aioCQsy3E", "Student Guide 101", "Cool stuff", -1, 10));
-        //repository.save(new Post("Frank", "story", "aioCQsy3E", "Student Guide 102", "Bad  stuff", -1, 2));
+        repository.save(new Post("Charlie", "story", "aioCQsy3E", "Student Guide 101", "Cool stuff", -1, 10));
+        repository.save(new Post("Frank", "story", "aioCQsy3E", "Student Guide 102", "Bad  stuff", -1, 2));
 //
 //        // fetch all articles
 //        System.out.println("Articles found with findAll():");
