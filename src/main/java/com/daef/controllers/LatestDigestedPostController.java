ac24b3e56afa4bf7183a72c32a8acae232bd07b1
Anders Bjergfelt <abj@dis-play.dk>
2017-11-13 23:09:53 +0100

    trying to fix memory

--- src/main/java/com/daef/controllers/LatestDigestedPostController.java
+++ src/main/java/com/daef/controllers/LatestDigestedPostController.java
@@ -8,9 +8,11 @@ package com.daef.controllers;
 import com.daef.models.Post;
 import com.daef.repositories.PostRepository;
 import java.util.List;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -26,16 +28,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/latest")
 public class LatestDigestedPostController {
    
-     @Autowired
+    @Autowired
+    MongoTemplate mongoTemplate;
+    
+    public LatestDigestedPostController() {
+        
+    }
+    
+    
+    @Autowired
     private PostRepository repository;
     
     
      @RequestMapping(method = RequestMethod.GET)
     public int getLatest(){
-        
-         System.out.println("Latest called");
-        List<Post> maxObject = repository.findAll(new Sort(Sort.Direction.DESC, "hanesstID"));
-        
+        Query query = new Query();
+        query.with(new Sort(Sort.Direction.DESC, "hanesstID"));
+        query.limit(5);
+        List<Post> maxObject = mongoTemplate.find(query, Post.class);
+        System.out.println("Latest called");
+        //List<Post> maxObject = repository.findAll(new Sort(Sort.Direction.DESC, "hanesstID"));
+        //List<Post> maxObject = repository.findOne(exmpl);
         int latest = !maxObject.isEmpty() ?  maxObject.get(0).getHanesstID() : 0;
         
          return latest;
