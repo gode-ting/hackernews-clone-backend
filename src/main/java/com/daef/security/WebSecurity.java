95923d3ced7b9cab8dedecf44811692f2d95d15a
Anders Bjergfelt <abj@dis-play.dk>
2017-11-28 14:28:40 +0100

    permit

--- src/main/java/com/daef/security/WebSecurity.java
+++ src/main/java/com/daef/security/WebSecurity.java
@@ -44,7 +44,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 //                .antMatchers(HttpMethod.POST, "/api/**").permitAll()
 //                .antMatchers(HttpMethod.PUT, "/api/**").permitAll()
 //                .antMatchers(HttpMethod.DELETE, "/api/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/post").permitAll()
+                .antMatchers(HttpMethod.POST, "/post").access("hasIpAddress('138.68.91.198')")
                 .antMatchers(HttpMethod.GET, "/status").permitAll()
                 .antMatchers(HttpMethod.GET, "/latest").permitAll()
                 .antMatchers(HttpMethod.POST, "api/post/comments/**").permitAll()
